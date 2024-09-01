eb4f9c616e7fe19446c246185d1bb5676ce56cdf
Edio Goncalves Junior <ediogoncalvesjr@hotmail.com>
2024-09-01 20:44:26 -0300

    Delete background call returned by the api

--- js/weather.js
+++ js/weather.js
@@ -72,9 +72,6 @@ const showWeatherData = async (city) => {
   umidityElement.innerText = `${data.main.humidity}%`;
   windElement.innerText = `${data.wind.speed}km/h`;
 
-  // Change bg image
-  document.body.style.backgroundImage = `url("${apiUnsplash + city}")`;
-
   weatherContainer.classList.remove("hide");
 };
 
