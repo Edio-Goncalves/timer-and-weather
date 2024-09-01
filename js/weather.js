1e19b7157b0505c2a3e83444fa868c3544a77b21
Edio Goncalves Junior <ediogoncalvesjr@hotmail.com>
2024-09-01 20:30:29 -0300

    Maps the elements

---
+++ js/weather.js
@@ -0,0 +1,14 @@
+const apiKey = "c1f55bac22040ebd59bd55ab2a85bc4d";
+const apiCountryURL = "https://countryflagsapi.com/png/";
+const apiUnsplash = "https://source.unsplash.com/1600x900/?";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const umidityElement = document.querySelector("#umidity span");
+const windElement = document.querySelector("#wind span");
